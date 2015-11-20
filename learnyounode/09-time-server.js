6dacb2b1c2a039a60a5a714b8e876a97dc4c7651
Jeff Meyer <jcmeyer10@gmail.com>
2015-11-20 17:13:15 -0500

    When they said adventerous, I think they meant, way more straightforward.  Fun to understand sockets and ending sockets and learning about srtftime!

---
+++ learnyounode/09-time-server.js
@@ -0,0 +1,10 @@
+var net = require('net');
+var strftime = require('strftime');
+
+var server = net.createServer(function (socket) {
+  var d = strftime('%F %k:%M');
+
+socket.end(d);
+});
+
+server.listen(process.argv[2]);
